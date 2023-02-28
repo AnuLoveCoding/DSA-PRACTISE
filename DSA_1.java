67b28c2e528ccf4ee1bb1eb28689d6a62b5b2ca1
anuLoveCoading <anusrivas07@gmail.com>
2023-03-01 03:40:29 +0530

    first commit

---
+++ DSA_1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class DSA_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> num = new ArrayList<>();
+        // Scanner input = new Scanner(System.in);
+        
+
+
+        //        some function in ArrayList;
+
+
+        //        add
+                num.add(10);
+                num.add(50);
+                num.add(60);
+            System.out.println(num);
+
+        //        add to index;
+
+
+
+        //        get;
+
+
+
+        //        set;
+
+
+
+        //        size();
+
+
+
+        //        remove;
+
+
+    }
+}
---
+++ Dsa_ArrayList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DSA_ARRAYLIST {
+    public static void main(String[] args) {
+        ArrayList<Integer> num = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        
+
+
+        //        some function in ArrayList;
+
+
+        //        add
+                num.add(10);
+                num.add(50);
+                num.add(60);
+            System.out.println(num);
+
+        //        add to index;
+
+
+
+        //        get;
+
+
+
+        //        set;
+
+
+
+        //        size();
+
+
+
+        //        remove;
+
+    }
+}
